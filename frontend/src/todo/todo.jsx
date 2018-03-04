688f06857c6c87a13164416a79afa3b0f4bb730c
Pedro Henrique <pedro.dorini@hotmail.com>
2018-03-03 21:32:37 -0300

    add todo component

---
+++ frontend/src/todo/todo.jsx
@@ -0,0 +1,11 @@
+import React, { Component }from 'react'
+
+export default class Todo extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Todo app</h1>
+      </div>
+    )
+  }
+}
