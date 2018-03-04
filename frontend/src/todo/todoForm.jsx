50031ab1f8f38200b336f160e5d2bf7d21dc6b6f
Pedro Henrique <pedro.dorini@hotmail.com>
2018-03-04 16:03:02 -0300

    add grid component

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import Grid from '../template/grid'
 
 export default props => (
   <div role="form" className="todoForm">
-    <div className="col-xs-12 col-sm-9 col-md-10">
-      <input id="description" className="form-control" placeHolder="Add a task" type="text"/>
-    </div>
-    <div className="col-xs-12 col-sm-3 col-md-2">
+    <Grid cols="12 9 10">
+      <input id="description" className="form-control" placeholder="Add a task" type="text"/>
+    </Grid>
+    <Grid cols="12 3 2">
       <button className="btn btn-primary"><i className="fa fa-plus"></i></button>
-    </div>
+    </Grid>
   </div>
 )
