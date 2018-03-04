b92b37a4699011c339ab3affc6bcfc373859c4eb
Pedro Henrique <pedro.dorini@hotmail.com>
2018-03-04 16:30:19 -0300

    add handleChange and handleAdd method to todo component

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -5,10 +5,11 @@ import IconButton from '../template/iconButton'
 export default props => (
   <div role="form" className="todoForm">
     <Grid cols="12 9 10">
-      <input id="description" className="form-control" placeholder="Add a task" type="text"/>
+      <input id="description" className="form-control" placeholder="Add a task" type="text"
+        value={props.description} onChange={props.handleChange}/>
     </Grid>
     <Grid cols="12 3 2">
-      <IconButton style='primary' icon='plus'></IconButton>
+      <IconButton style='primary' icon='plus' onClick={props.handleAdd}></IconButton>
     </Grid>
   </div>
 )
